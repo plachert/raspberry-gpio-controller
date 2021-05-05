d363cd6014ae90c7b39e844f5b08010420432537
plachert <pmlachert@gmail.com>
2021-05-05 16:36:51 +0200

    Added pinboard to flask. Minor refactoring

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -1,7 +1,17 @@
 from flask import current_app as app
 from flask import render_template
+from raspi.raspi_board import PinBoard
+
+@app.before_first_request
+def startup():
+    global pinboard
+    pinboard = PinBoard()
 
 @app.route("/")
 def raspi():
-    print("DFASFASDFASD")
+    print(pinboard.get_state())
     return render_template("raspi.html")
+
+@app.route("/background_process")
+def background_process():
+    pass
